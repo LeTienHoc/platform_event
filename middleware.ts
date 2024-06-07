f198e64185f93270dd25684c312846b7eedc5b2a
tienhoc <letienhocpy363@gmail.com>
2024-06-07 21:59:54 +0700

    fix

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,14 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher([
   '/dashboard(.*)',
   '/forum(.*)',
   '/api/webhooks(.*)'
 ]);
-
 export default clerkMiddleware((auth, req) => {
-  if (!auth().userId && isProtectedRoute(req)) {
-
-    // Add custom logic to run before redirecting
-
-    return auth().redirectToSignIn();
-  }
+  if (isProtectedRoute(req)) auth().protect();
 });
 
-export const config = { matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)']};
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/"],
+};
