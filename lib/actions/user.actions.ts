cf0154306075624df26590b06e8ecb043b88e808
tienhoc <letienhocpy363@gmail.com>
2024-06-09 16:29:18 +0700

    thêm async

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -79,7 +79,7 @@ export async function deleteUser(clerkId: string) {
     handleError(error)
   }
 }
-export function removeVietnameseTones(str:string) {
+export async function removeVietnameseTones(str:string) {
   str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g,"a"); 
   str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g,"e"); 
   str = str.replace(/ì|í|ị|ỉ|ĩ/g,"i"); 
